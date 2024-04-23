0917b50ccaa51e90ec4673f8171a663a4e0720fe
FaheemIot <149452883+FaheemIot@users.noreply.github.com>
2024-04-23 11:37:20 -0700

    Add files via upload

---
+++ modern_python/day 01/welcome.py
@@ -0,0 +1,2 @@
+print("welcome to python")
+print("faheem","nice to met you")
---
+++ modern_python/day 02/age.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    print(i)
+age = int(input("Please enter your age: "))
+
+# Checking if the age is even or odd
+if age % 2 == 0:
+    print("Your age", age, "is an even number.")
+else:
+    print("Your age", age, "is an odd number.")
